68db21ae23f5222e84de89efcc03e6321e5de15e
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-01-10 22:43:55 +0100

    update

--- src/homepage/index.tsx
+++ src/homepage/index.tsx
@@ -1,32 +1,126 @@
 /* eslint-disable react/no-unescaped-entities */
 import { HomepageContainer } from "./styles";
+import printf from "../assets/ printf.png";
+import shell from "../assets/ Simple Shell.png";
+import airbnb from "../assets/ Airbnb.png";
+import afrohair from "../assets/Banner-afrohair.png";
 
 
 export function Homepage(){
   return(
     < HomepageContainer>
-      <p>Hi, I'm Queise Carvalho</p>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
-      <h1>Hi, I'm Queise Carvalho</h1>
+      <h1>Projects</h1>
+      <div className="carousel prev">
+        <div className="list">
+          <div className="item">
+            <img src={printf} alt="printf"/>
+            <div className="content">
+              <div className="title">Project PRINTF</div>
+              <div className="topic">Project d’Evaluation fin du 1° trimestre</div>
+              <div className="tools">C</div>
+              <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reiciendis porro, velit facere, voluptatibus quos possimus sapiente, impedit explicabo earum maxime nihil voluptate voluptatum culpa mollitia nesciunt qui recusandae doloremque.
+              </div>
+              <div className="buttom">
+                <button>Github</button>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={shell} alt="shell"/>
+            <div className="content">
+              <div className="title">Project Simple Shell</div>
+              <div className="topic">Project d’Evaluation fin du 1° trimestre</div>
+              <div className="tools">C</div>
+              <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reiciendis porro, velit facere, voluptatibus quos possimus sapiente, impedit explicabo earum maxime nihil voluptate voluptatum culpa mollitia nesciunt qui recusandae doloremque.
+              </div>
+              <div className="buttom">
+                <button>Github</button>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={airbnb} alt="airbnb"/>
+            <div className="content">
+              <div className="title">Project AirBNB Clone</div>
+              <div className="topic">Project d’Evaluation fin du 2° trimestre</div>
+              <div className="tools">
+                Pyhton - Flask - MySQL - JavaScript - HTML- CSS
+              </div>
+              <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reiciendis porro, velit facere, voluptatibus quos possimus sapiente, impedit explicabo earum maxime nihil voluptate voluptatum culpa mollitia nesciunt qui recusandae doloremque.
+              </div>
+              <div className="buttom">
+                <button>Github</button>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={afrohair} alt="afrohair"/>
+            <div className="content">
+              <div className="title">Projet AFROHAIR</div>
+              <div className="topic">Project Portfolio Frontend et Backend</div>
+              <div className="tools">
+                Frontend - HTML - CSS - React - TypeScript
+                Backend - Node.js - Knex.js - SQlite
+              </div>
+              <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem reiciendis porro, velit facere, voluptatibus quos possimus sapiente, impedit explicabo earum maxime nihil voluptate voluptatum culpa mollitia nesciunt qui recusandae doloremque.
+              </div>
+              <div className="buttom">
+                <button>Github</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="thumbnail">
+          <div className="item">
+            <img src={printf} alt="printf"/>
+            <div className="content">
+              <div className="title">
+                Name Slider
+              </div>
+              <div className="description">
+                descirption
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={shell} alt="shell"/>
+            <div className="content">
+              <div className="title">
+                Name Slider
+              </div>
+              <div className="description">
+                descirption
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={airbnb} alt="airbnb"/>
+            <div className="content">
+              <div className="title">
+                Name Slider
+              </div>
+              <div className="description">
+                descirption
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={afrohair} alt="afrohair"/>
+            <div className="content">
+              <div className="title">
+                Name Slider
+              </div>
+              <div className="description">
+                descirption
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="arrows">
+          <button id="prev">&lt;</button>
+          <button id="next">&gt;</button>
+        </div>
+      </div>
     </HomepageContainer>
   );
 }
--- src/homepage/styles.tsx
+++ src/homepage/styles.tsx
@@ -2,5 +2,222 @@ import styled from "styled-components";
 
 export const HomepageContainer = styled.div`
 
+.carousel {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+}
+
+.carousel .list .item {
+    position: absolute;
+    inset: 0 0 0 0;
+}
+
+.carousel .list .item img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.carousel .list .item .content {
+    position: absolute;
+    top: 20%;
+    width: 1140px;
+    max-width: 80%;
+    left: 50%;
+    transform: translateX(-50%);
+    padding-right: 30%;
+    box-sizing: border-box;
+    color: #fff;
+    text-shadow: 0 5px 10px #000400;
+}
+
+.carousel .list .item .content .title {
+    font-weight: bold;
+    letter-spacing: 10px;
+}
+
+.carousel .list .item .content .topic,
+.carousel .list .item .content .tools {
+    font-weight: bold;
+    font-size: 1em;
+    line-height: 1.3em;
+
+}
+
+/* thumbnail
+----------------*/
+
+.thumbnail {
+    position: absolute;
+    bottom: 50px;
+    left: 50%;
+    width: max-content;
+    z-index: 100;
+    display: flex;
+    gap: 20px;
+}
+
+.thumbnail .item {
+    width: 150px;
+    height: 220px;
+    flex-shrink: 0;
+    position: relative;
+}
+
+.thumbnail .item img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 20px;
+
+}
+
+.thumbnail .item .content {
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    right: 10px;
+}
+
+.thumbnail .item .content .title {
+    font-weight: bold;
+}
+
+/* arrow 
+-----------*/
+
+.arrows {
+    position: absolute;
+    top: 80%;
+    right: 52%;
+    width: 300px;
+    max-width: 30%;
+    display: flex;
+    gap: 10px;
+    align-items: center;    
+}
+
+.arrows button {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%; 
+    background-color: #eee4;
+    border: none;
+    font-family: monospace;
+    color: #fff;
+    font-weight: bold;
+    font-size: large;
+    transition: .5s;
+    z-index: 100;
+}
+
+.arrows button:hover {
+    background-color: #eee;
+    color: #555;
+}
+
+.carousel .list .item:nth-child(1) {
+    z-index: 1;
+}
+
+.carousel .list .item:nth-child(1) .title,
+.carousel .list .item:nth-child(1) .topic,
+.carousel .list .item:nth-child(1) .tools,
+.carousel .list .item:nth-child(1) .description,
+.carousel .list .item:nth-child(1) .buttom {
+    transform: translateY(50px);
+    filter: blur(20px);
+    opacity: 0;
+    animation: showContent 0.5s 1s linear 1 forwards;
+}
+
+@keyframes showContent {
+    to {
+        opacity: 1;
+        filter: blur(0);
+        transform: translateY(0);
+    }
+}
+
+.carousel .list .item:nth-child(1) .topic {
+    animation-delay: 1.2s;
+}
+
+.carousel .list .item:nth-child(1) .tools {
+    animation-delay: 1.4s;
+}
+
+.carousel .list .item:nth-child(1) .description {
+    animation-delay: 1.6s;
+}
+
+.carousel .list .item:nth-child(1) .buttom {
+    animation-delay: 1.8s;
+}
+
+/* effect next click 
+-------------------------*/
+
+.carousel.next .list .item:nth-child(1) img {
+    width: 150px;
+    height: 220px;
+    position: absolute;
+    left: 50%;
+    bottom: 50px;
+    border-radius: 20px;
+    animation: showImage 0.5s linear 1 forwards;
+}
+
+@keyframes showImage {
+    to {
+        width: 100%;
+        height: 100%;
+        left: 0;
+        bottom: 0;
+        border-radius: 0;
+    }
+}
+
+.carousel.next .thumbnail .item:nth-last-child(1) {
+    width: 0;
+    overflow: hidden;
+    animation: showThumbnail .5s linear 1 forwards;
+}
+
+@keyframes showThumbnail {
+    to {
+        width: 150px;
+    }
+}
+
+.carousel.next .thumbnail {
+    transform: translateX(150px);
+    animation: transformThumbnail .5s linear 1 forwards;
+}
+
+@keyframes transformThumbnail {
+    to{
+        transform: translateX(0);
+    }    
+}
+
+/* effect prev clic */
+.carousel.prev .list .item:nth-child(2) {
+    z-index: 2;
+
+}
+.carousel.prev .list .item:nth-child(2) img{
+    position: absolute;
+    bottom:0;
+    left: 0;
+    animation: outImage 0.5s linear 1 forwards;
+
+}
+@keyframes outImage{
+    
+}
+
 
 `;
